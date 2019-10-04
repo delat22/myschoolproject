ed3e580d8be328566bf6d0f87aee7ec92b193bde
delat22 <39204293+delat22@users.noreply.github.com>
2019-10-04 03:28:53 +0100

    Add files via upload

---
+++ uniquenews - Copy/news.php
@@ -0,0 +1,120 @@
+<?php 
+    $title = "UIS - News";
+    $css = "news";
+    include "php/nav.php";
+?>
+
+<main>
+    <section class="news-head">
+        <section class="news-left">
+            <h1>What's Happening In Our School ?</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua.</p>
+        </section>  
+        <button>Read On!</button>
+    </section>
+    
+    <section class="headline">
+        <h2>News Room</h2>
+        <section class="headline-cont">
+            <section class="headline-row">
+                <section class="headline-col">
+                    <section class="headline-col-post">
+                        <img src="images/adult.jpg">
+                        <article>
+                            <h3>This is today's News</h3>
+                            <p>Blah, blah, blah, today, this this and this happened. It is such a happy occasion for the school, yah.</p>
+                            <ul>
+                                <li><a href=""><em></em> Author</a></li>
+                                <li><a href=""><em></em> Read More &rarr;</a></li>
+                            </ul>
+                        </article>
+                    </section>
+                    <section class="headline-col-post">
+                        <img src="images/adult.jpg">
+                        <article>
+                            <h3>This is today's News</h3>
+                            <p>Blah, blah, blah, today, this this and this happened. It is such a happy occasion for the school, yah.</p>
+                            <ul>
+                                <li><a href=""><em></em> Author</a></li>
+                                <li><a href=""><em></em> Read More &rarr;</a></li>
+                            </ul>
+                        </article>
+                    </section>
+                    <section class="headline-col-post">
+                        <img src="images/adult.jpg">
+                        <article>
+                            <h3>This is today's News</h3>
+                            <p>Blah, blah, blah, today, this this and this happened. It is such a happy occasion for the school, yah.</p>
+                            <ul>
+                                <li><a href=""><em></em> Author</a></li>
+                                <li><a href=""><em></em> Read More &rarr;</a></li>
+                            </ul>
+                        </article>
+                    </section>
+                    <section class="headline-col-post">
+                        <img src="images/adult.jpg">
+                        <article>
+                            <h3>This is today's News</h3>
+                            <p>Blah, blah, blah, today, this this and this happened. It is such a happy occasion for the school, yah.</p>
+                            <ul>
+                                <li><a href=""><em></em> Author</a></li>
+                                <li><a href=""><em></em> Read More &rarr;</a></li>
+                            </ul>
+                        </article>
+                    </section>
+                </section>
+                <section class="headline-col">
+                    <section class="headline-col-post">
+                        <img src="images/adult.jpg">
+                        <article>
+                            <h3>This is today's News</h3>
+                            <p>Blah, blah, blah, today, this this and this happened. It is such a happy occasion for the school, yah.</p>
+                            <ul>
+                                <li><a href=""><em></em> Author</a></li>
+                                <li><a href=""><em></em> Read More &rarr;</a></li>
+                            </ul>
+                        </article>
+                    </section>
+                    <section class="headline-col-post">
+                        <img src="images/adult.jpg">
+                        <article>
+                            <h3>This is today's News</h3>
+                            <p>Blah, blah, blah, today, this this and this happened. It is such a happy occasion for the school, yah.</p>
+                            <ul>
+                                <li><a href=""><em></em> Author</a></li>
+                                <li><a href=""><em></em> Read More &rarr;</a></li>
+                            </ul>
+                        </article>
+                    </section>
+                    <section class="headline-col-post">
+                        <img src="images/adult.jpg">
+                        <article>
+                            <h3>This is today's News</h3>
+                            <p>Blah, blah, blah, today, this this and this happened. It is such a happy occasion for the school, yah.</p>
+                            <ul>
+                                <li><a href=""><em></em> Author</a></li>
+                                <li><a href=""><em></em> Read More &rarr;</a></li>
+                            </ul>
+                        </article>
+                    </section>
+                    <section class="headline-col-post">
+                        <img src="images/adult.jpg">
+                        <article>
+                            <h3>This is today's News</h3>
+                            <p>Blah, blah, blah, today, this this and this happened. It is such a happy occasion for the school, yah.</p>
+                            <ul>
+                                <li><a href=""><em></em> Author</a></li>
+                                <li><a href=""><em></em> Read More &rarr;</a></li>
+                            </ul>
+                        </article>
+                    </section>
+                </section>
+            </section>
+        </section>
+    </section>
+</main>
+
+<?php 
+    include "php/footer.php";
+?>
---
+++ uniquenews - Copy/sign-in.php
@@ -0,0 +1,49 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+  <title></title>
+  <link rel="stylesheet" type="text/css" href="css/form.css">
+</head>
+<body>
+    <main>
+  <div id="wrapper">
+        <div id="left">
+          <div id="signin">
+            <div class="logo">
+              <a href="index.php"><img src="images/logo.png" alt="UNIS" /></a>
+            </div>
+            <form>
+              <div>
+                <label>Email or username</label>
+                <input type="text" class="text-input" />
+              </div>
+              <div>
+                <label>Password</label>
+                <input type="password" class="text-input" />
+              </div>
+              <button type="submit" class="primary-btn">Sign In</button>
+            </form>
+            <div class="links">
+              <a href="#">Forgot Password</a>
+              <a href="Sign-up.php">Sign up</a>
+            </div>
+
+          </div>
+
+        </div>
+        <div id="right">
+          <div id="showcase">
+            <div class="showcase-content">
+              <h1 class="showcase-text">
+              Enroll your child/ward <strong>today</strong>
+              </h1>
+              <a href="sign-up.php" class="secondary-btn">Register now</a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+</main>
+</body>
+</html>
